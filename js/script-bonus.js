eb53d00099b04657f621ce28e34f9ce611a70ece
adele <adele.r87@gmail.com>
2020-12-11 15:20:32 +0100

    esercizio bonus

---
+++ js/script-bonus.js
@@ -0,0 +1,12 @@
+// creo la viariabile nome e la faccio come domanda
+var nome = prompt('Come ti Chiami?');
+// dove andra a stamparmi le risposte
+var outputBox = document.getElementById('output');
+
+// se la risposta è uguale a quella allora stampami ciao adele senno stampami che non mi conosci
+
+if(nome === 'Adele') {
+  outputBox.append('Ciao ' + nome)
+} else {
+  outputBox.append('non ti conosco.')
+}
