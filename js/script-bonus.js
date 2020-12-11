4dc1b5d69890625e755866d90b11b267fd29d6c3
adele <adele.r87@gmail.com>
2020-12-11 15:35:17 +0100

    sperimento morra cinese

--- js/script-bonus.js
+++ js/script-bonus.js
@@ -1,12 +1,22 @@
-// creo la viariabile nome e la faccio come domanda
-var nome = prompt('Come ti Chiami?');
-// dove andra a stamparmi le risposte
+// // creo la viariabile nome e la faccio come domanda
+// var nome = prompt('Come ti Chiami?');
+// // dove andra a stamparmi le risposte
+// var outputBox = document.getElementById('output');
+//
+// // se la risposta è uguale a quella allora stampami ciao adele senno stampami che non mi conosci
+//
+// if(nome === 'Adele') {
+//   outputBox.append('Ciao ' + nome)
+// } else {
+//   outputBox.append('non ti conosco.')
+// }
+
+
+// carta forbici sasso
+
+var domanda =prompt('carta,mani,forbici?')
 var outputBox = document.getElementById('output');
 
-// se la risposta è uguale a quella allora stampami ciao adele senno stampami che non mi conosci
-
-if(nome === 'Adele') {
-  outputBox.append('Ciao ' + nome)
-} else {
-  outputBox.append('non ti conosco.')
+if( domanda==='forbici'){
+  outputBox.append('sasso' + '\nhai perso!')
 }
