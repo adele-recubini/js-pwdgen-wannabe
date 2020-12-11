143173103d176ae36ea55909a17bcc63f3f09172
adele <adele.r87@gmail.com>
2020-12-11 14:47:18 +0100

    fine esercizio classico

--- js/script.js
+++ js/script.js
@@ -5,33 +5,13 @@ var cognome = prompt('Il tuo cognome?');
 var colorePreferito = prompt('Il tuo colore preferito?');
 
 // ora creo la variabile per la data corrente
-// var annoCorrente = new Date().getFullYear();
-// var annoDiNascita = annoCorrente - age;
+var age = 33;
+var annoCorrente = new Date().getFullYear();
+var annoDiNascita = annoCorrente - age;
 
 // ora queste risposte dovranno essere stampante sul mio html del div output cosi da vederle stampate
 
 var outputBox = document.getElementById('output');
 // outputBox.innerText= 'nome' + 'congome' + 'colorePreferito'; sbagliato
 
-outputBox.append(nome + cognome + colorePreferito + 19);
-
-
-
-
-
-
-
-
-// var risposta = prompt('Come ti chiami?');
-// console.log('Il tizio si chiama: ' + risposta);
-// // document.writeln(risposta);
-// var outputContainer = document.getElementById('output');
-// outputContainer.innerText = 'ciao ' + risposta;
-// // funzione per "appendere", aggiungere, alla variabile outputContainer il suo argomento.
-// outputContainer.append('\nquesto è un append');  // diverso da innerHTML, perché lo "aggiunge".
-// //^variabile    ^funzione        ^argomento/parametro
-// var age = prompt(' Ciao ' + risposta + '. Quanti anni hai?');
-// console.log('Il tizio ha: ' + age + ' anni');
-// var annoCorrente = new Date().getFullYear();
-// var annoDiNascita = annoCorrente - age;
-// outputContainer.append('\nla tua data di nascita è ' + annoDiNascita);
+outputBox.append(nome + cognome + colorePreferito + annoCorrente);
